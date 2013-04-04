32df4557c73da62485bc43dc730a90bc6b71b826
Sarah Fong <sarah.fong@gmail.com>
2013-04-04 18:03:59 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BidLounge::Application.config.secret_token = '499d0f4208ce886a3f7cf4819b679cd0d6e2e5db6fdf842992aa2a1bb93ffbdd774eea3328c5a7a278deb7ce65888df49ddfd742cb5719df9fb2802499315efe'
